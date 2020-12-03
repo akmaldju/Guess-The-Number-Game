d17b41c122b074763ed431ea3913e6a28f1a2a32
Akmal Djumakhodjaev <33540033+akmaldju@users.noreply.github.com>
2020-12-03 22:40:34 +0800

    Refactor: Added jQuery

--- game.js
+++ game.js
@@ -1,47 +1,43 @@
 var number = 0;
 var ok;
 function startGame() {
-	document.getElementById('dialog').innerText =
-		'Hello, I want to play a game with you...';
+	$('#dialog').text('Hello, I want to play a game with you...');
 	setTimeout(intro1, 3000);
 	return false;
 }
 function intro1() {
-	document.getElementById('dialog').innerText =
-		'The rules are simple: I have a number in mind and you need to guess which number it is';
+	$('#dialog').text('The rules are simple: I have a number in mind and you need to guess which number it is');
 	setTimeout(intro2, 5000);
 	number = Math.floor(Math.random() * 1000) + 1;
 	// console.log(number);
 	return false;
 }
 function intro2() {
-	document.getElementById('dialog').innerText =
-		'Every time you say a wrong number I will tell you if your number is bigger or smaller than mine';
+	$('#dialog').text('Every time you say a wrong number I will tell you if your number is bigger or smaller than mine');
 	setTimeout(intro3, 7000);
 	return false;
 }
 function intro3() {
-	document.getElementById('dialog').innerText = 'So, let us start';
+	$('#dialog').text('So, let us start');
 	return false;
 }
-
-window.onload = function () {
+$(document).ready(function () {
 	startGame();
-	ok = document.getElementById('button');
-	ok.onclick = function () {
+	ok = $('#button');
+	ok.click(function () {
 		// console.log('Click');
-		// console.log(document.getElementById('answer').value);
-		let userGuess = document.getElementById('answer').value;
+		// console.log($('#answer'));
+		let userGuess = $('#answer').val();
 		if (userGuess == number) {
-			document.getElementById('dialog').innerText = 'You won! Try again...';
+			$('#dialog').text('You won! Try again...');
 			number = Math.floor(Math.random() * 1000) + 1;
 		} else if (userGuess > number) {
-			document.getElementById('dialog').innerText = 'Your number is bigger';
+			$('#dialog').text('Your number is bigger');
 		} else if (userGuess < number) {
-			document.getElementById('dialog').innerText = 'Your number is smaller';
+			$('#dialog').text('Your number is smaller');
 		} else {
-			document.getElementById('dialog').innerText = 'Something is wrong';
+			$('#dialog').text('Something is wrong');
 		}
 		return false;
-	};
-};
+	});
+});
