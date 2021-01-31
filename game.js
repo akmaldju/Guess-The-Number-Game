b4099c8b9f5b0b4ac842cf71eb0ab387ba2324c9
akmaldju <33540033+akmaldju@users.noreply.github.com>
2021-01-31 21:47:00 +0800

    Added dificulty settings
    - Added max possible number selection
    - Fix: number wrongly changing after every input

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ const dialog = '#dialog';
 const btnOk = '#button';
 
 let number = 0;
+let maxNumber = 0;
+let gameStarted = false;
 
 const startGame = () => {
 	$(dialog).text('Hello, I want to play a game with you...');
@@ -12,7 +14,7 @@ const startGame = () => {
 const intro1 = () => {
 	$(dialog).text('The rules are simple: I have a number in mind and you need to guess which number it is');
 	setTimeout(intro2, 5000);
-	number = Math.floor(Math.random() * 1000) + 1;
+	// number = Math.floor(Math.random() * 1000) + 1;
 	return false;
 }
 
@@ -24,16 +26,30 @@ const intro2 = () => {
 
 const intro3 = () => {
 	$(dialog).text('So, let us start');
+	setTimeout(changeDifficulty, 9000);
 	return false;
 }
 
-$(document).ready(() => {
-	startGame();
+const changeDifficulty = () => {
+	$(dialog).text('First, type the maximum number I can choose from');
+	$(btnOk).click(() => {
+		if (!gameStarted) {
+			maxNumber = $('#answer').val();
+			number = Math.floor(Math.random() * maxNumber) + 1;
+			game();
+			gameStarted = true;
+			return false;
+		}
+	});
+	return false;
+}
+
+const game = () => {
 	$(btnOk).click(() => {
 		const userGuess = $('#answer').val();
 		if (userGuess == number) {
 			$(dialog).text('You won! Try again...');
-			number = Math.floor(Math.random() * 1000) + 1;
+			number = Math.floor(Math.random() * maxNumber) + 1;
 		} else if (userGuess > number) {
 			$(dialog).text('Your number is bigger');
 		} else if (userGuess < number) {
@@ -43,4 +59,8 @@ $(document).ready(() => {
 		}
 		return false;
 	});
+}
+
+$(document).ready(() => {
+	startGame();
 });
